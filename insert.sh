06d79ec5abf00194082f1c30bf396598b6d500da
James Clark <james.clark@ligo.org>
2018-03-30 10:51:22 -0700

    insert script uses rucio checksum tools, remove Gfal2 dependency

--- insert.sh
+++ insert.sh
@@ -5,17 +5,17 @@ source ${PWD}/etc/rucio-user-env.sh
 # Times taken from:
 # https://wiki.ligo.org/LSC/JRPComm/EngRun8
 
-#start_time=1126623617 # start of O1
-#end_time=$((${start_time}+5000))
-start_time=1123858817 # start of ER8 
-end_time=1126623617 # end of ER8
+start_time=1126623617 # start of O1
+end_time=$((${start_time}+1000))
+#start_time=1123858817 # start of ER8 
+#end_time=1126623617 # end of ER8
 #end_time=$((${start_time}+1000))
 
 RUCIO_ACCOUNT=root ./insert_lvc_dataset.py \
     --log-file H1_HOFT_C02-${start_time}_${end_time}.log \
     --dataset H1_HOFT_C02-${start_time}_${end_time} \
-    --scope ER8 \
-    --rse LIGOTEST \
+    --scope O1 \
+    --rse LIGOTESTCIT \
     --debug \
     --gps-start-time ${start_time} \
     --gps-end-time ${end_time} \
