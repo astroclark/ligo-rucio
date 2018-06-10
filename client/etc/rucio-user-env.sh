d090305397f9bf72910d56a9548e0c9261ae530a
James Alexander Clark <james.clark@ligo.org>
2018-06-10 16:30:42 -0400

    Environment / build scripts

--- client/etc/rucio-user-env.sh
+++ client/etc/rucio-user-env.sh
@@ -1,5 +1,5 @@
-export RUCIO_HOME=/home/jclark/Projects/ligo-rucio
+export RUCIO_HOME=/home/jclark/Projects/ligo-rucio/client
 export RUCIO_ACCOUNT=root
-source /home/jclark/opt/lscsoft/pycbc/etc/pycbc-user-env.sh
-source /home/jclark/opt/lscsoft/pycbc-glue/etc/glue-user-env.sh
+#source /home/jclark/opt/lscsoft/pycbc/etc/pycbc-user-env.sh
+#source /home/jclark/opt/lscsoft/pycbc-glue/etc/glue-user-env.sh
 export PYTHONPATH=${PYTHONPATH}:${RUCIO_HOME}/lib
