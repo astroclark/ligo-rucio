83a54163ae8893afac8b1957c19128876e00d854
James Clark <james.clark@ligo.org>
2018-07-20 16:02:14 -0400

    RUCIO_HOME needed in client env

--- client/etc/rucio-user-env.sh
+++ client/etc/rucio-user-env.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 source /home/jclark/opt/lscsoft/pycbc/etc/pycbc-user-env.sh
 source /home/jclark/opt/lscsoft/pycbc-glue/etc/glue-user-env.sh
+export RUCIO_HOME=/home/jclark/Projects/ligo-rucio/client
 export RUCIO_ACCOUNT=jclark
