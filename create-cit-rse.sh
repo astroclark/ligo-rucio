a4a380dc30f91119b926bbc9f85704a5bcaa8f6c
James Alexander Clark <james.clark@ligo.org>
2018-08-02 13:56:47 -0700

    moved delegation script

--- create-cit-rse.sh
+++ create-cit-rse.sh
@@ -4,8 +4,8 @@ rucio-admin rse add LIGO-CIT
 
 rucio-admin rse add-protocol\
     --prefix /home/jclark/Projects/ligo-rucio/data \
+    --hostname ldas-pcdev2.ligo.caltech.edu\
     --domain-json '{"wan": {"read": 1, "write": 1, "delete": 1, "third_party_copy": 1}}' \
     --scheme gsiftp \
-    --hostname ldas-pcdev2.ligo.caltech.edu\
     --port 2811 \
     LIGO-CIT
