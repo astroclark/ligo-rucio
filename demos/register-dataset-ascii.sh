ba09ac98e70b4763998ee87351f65b9ae59bbd72
James Clark <james.clark@ligo.org>
2018-08-16 06:10:04 -0700

    updating command line call to register ascii

--- demos/register-dataset-ascii.sh
+++ demos/register-dataset-ascii.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Get current pycbc
-source ${PWD}/etc/rucio-user-env.sh
-
+rucio_repo=/home/jclark/Projects/ligo-rucio
+source ${rucio_repo}/client/etc/rucio-user-env.sh
 
 ./mini_dataset.py \
     --rse LIGO-CIT \
     --dataset-name test-dataset-0 \
     --debug --verbose \
-    --file-list data-insertion/test-dataset-0.dat
+    --file-list /home/jclark/Projects/ligo-rucio/data/test-dataset-0.dat
