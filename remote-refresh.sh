7cda1481ab490acab215ab612ba56d756354e197
James Clark <james.clark@ligo.org>
2018-08-16 08:33:54 -0400

    Incremental change in testing

--- remote-refresh.sh
+++ remote-refresh.sh
@@ -3,4 +3,5 @@ git commit -a -m "Incremental change in testing"
 git push
 #ssh james.clark@rucio.ligo.caltech.edu update-repo.sh
 ssh james.clark@rucio.ligo.caltech.edu "pushd ligo-rucio && git pull && popd"
+gsissh ldas-pcdev6.ligo.caltech.edu "pushd Projects/ligo-rucio && git pull && popd"
 
