665f2e0adc076d0f9b4d1b195d0ae6f4bc848771
James Clark <james.clark@ligo.org>
2018-08-21 08:57:32 -0400

    added virtualenv activation to user env script

--- client/etc/rucio-user-env.sh
+++ client/etc/rucio-user-env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # source /home/jclark/opt/lscsoft/pycbc/etc/pycbc-user-env.sh
 # source /home/jclark/opt/lscsoft/pycbc-glue/etc/glue-user-env.sh
+source ${HOME}/virtualenvs/ligo-rucio/bin/activate
 export RUCIO_HOME=/home/jclark/Projects/ligo-rucio/client
 export RUCIO_ACCOUNT=jclark
-export PYTHONPATH=/home/jclark/Projects/ligo-rucio/client/lib
+export PYTHONPATH=/home/jclark/Projects/ligo-rucio/ligo_lfn2pfn:${PYTHONPATH}
