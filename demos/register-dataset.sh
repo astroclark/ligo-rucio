99a076827fe5fa86a2a8ad8f665a2a7d6e97b8ab
James Clark <james.clark@ligo.org>
2018-08-22 15:24:34 -0400

    example comment about making dataset file

--- demos/register-dataset.sh
+++ demos/register-dataset.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-rucio_repo=/home/jclark/Projects/ligo-rucio
-source ${rucio_repo}/client/etc/rucio-user-env.sh
+#!/bin/sh -e
 
-${rucio_repo}/demos/register-dataset-ascii.py \
+# Get ascii list of frames with e.g., ls -d -1 $PWD/**/*gwf.  Ideally use DiskCache.
+ligo_register_dataset \
     --rse LIGO-CIT \
     --dataset-name test-dataset-0 \
     --debug --verbose \
