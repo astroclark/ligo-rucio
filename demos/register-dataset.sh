077f197453d3e2c52c997e6df574938ad9e62239
James Clark <james.clark@ligo.org>
2018-09-13 01:07:53 -0700

    cleaning

--- demos/register-dataset.sh
+++ demos/register-dataset.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
 # Get ascii list of frames with e.g., ls -d -1 $PWD/**/*gwf.  Ideally use DiskCache.
+# Better yet: find $PWD -type f -name *gwf
 ligo_register_dataset \
     --rse LIGO-CIT \
     --lifetime 600 \
     --dataset-name test-dataset-1 \
     --debug --verbose \
-    --file-list /home/jclark/Projects/ligo-rucio/data/test-filelist.dat
+    --file-list /home/jclark/Projects/ligo-rucio/data/test-filelist.txt
