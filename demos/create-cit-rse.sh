abe2e9817c615ef9a617e9d38510327a1f94b11e
James Alexander Clark <james.clark@ligo.org>
2018-09-14 02:33:13 -0700

    updating RSE creation scripts

--- demos/create-cit-rse.sh
+++ demos/create-cit-rse.sh
@@ -1,11 +1,25 @@
 #!/bin/sh -e
 
+# Create RSE
 rucio-admin rse add LIGO-CIT
 
+# Define copying protocol
 rucio-admin rse add-protocol\
-    --prefix /home/jclark/Projects/ligo-rucio/data \
+    --prefix /home/rucio \
     --hostname ldas-pcdev2.ligo.caltech.edu\
     --domain-json '{"wan": {"read": 1, "write": 1, "delete": 1, "third_party_copy": 1}}' \
     --scheme gsiftp \
     --port 2811 \
     LIGO-CIT
+
+
+# FTS configuration
+rucio-admin rse set-attribute --rse LIGO-CIT --key fts --value https://fts.mwt2.org:8446
+rucio-admin rse set-attribute --rse LIGO-CIT --key fts_testing --value https://fts.mwt2.org:8446
+
+# Disk quota
+rucio-admin -a root account set-limits root LIGO-CIT 1000000000000
+
+# Make this part of the LIGO lab network
+rucio-admin rse set-attribute --rse LIGO-CIT --key ligo_lab --value True
+
--- demos/create-lho-rse.sh
+++ demos/create-lho-rse.sh
@@ -1,11 +1,25 @@
 #!/bin/sh -e
 
-rucio-admin rse add LIGO-WA
+# Create RSE
+rucio-admin rse add LIGO-LHO
 
+# Define copying protocol
 rucio-admin rse add-protocol\
-    --prefix /home/jclark/Projects/ligo-rucio/data \
+    --prefix /hdfs/rucio \
     --hostname ldas-pcdev2.ligo-wa.caltech.edu\
     --domain-json '{"wan": {"read": 1, "write": 1, "delete": 1, "third_party_copy": 1}}' \
     --scheme gsiftp \
     --port 2811 \
-    LIGO-WA
+    LIGO-LHO
+
+
+# FTS configuration
+rucio-admin rse set-attribute --rse LIGO-LHO --key fts --value https://fts.mwt2.org:8446
+rucio-admin rse set-attribute --rse LIGO-LHO --key fts_testing --value https://fts.mwt2.org:8446
+
+# Disk quota
+rucio-admin -a root account set-limits root LIGO-LHO 1000000000000
+
+# Make this part of the LIGO lab network
+rucio-admin rse set-attribute --rse LIGO-LHO --key ligo_lab --value True
+
