c1b8ec962d775e2a13f9497df6c7c6f429ad01d9
James Clark <james.clark@ligo.org>
2018-09-14 06:32:50 -0400

    updating reg demo

--- demos/register-dataset.sh
+++ demos/register-dataset.sh
@@ -3,8 +3,8 @@
 # Get ascii list of frames with e.g., ls -d -1 $PWD/**/*gwf.  Ideally use DiskCache.
 # Better yet: find $PWD -type f -name *gwf
 ligo_register_dataset \
-    --rse LIGO-CIT \
-    --scope TEST \
-    --dataset-name L-L1_HOFT_C00-11370b \
+    --rse LIGO-CIT-OFFICIAL \
+    --scope O1 \
+    --dataset-name L-L1_HOFT_C00-11370 \
     --debug --verbose \
-    --file-list /home/jclark/Projects/ligo-rucio/data/L-L1_HOFT_C00-11370.txt
+    --file-list /home/jclark/Projects/ligo-rucio/data/O1:L-L1_HOFT_C00-11370.txt
