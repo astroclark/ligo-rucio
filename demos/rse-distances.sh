4b512583b0c21d208adf7a112dcfd2eaa1bbc189
James Alexander Clark <james.clark@ligo.org>
2018-09-15 02:04:09 -0700

    updated distances

--- demos/rse-distances.sh
+++ demos/rse-distances.sh
@@ -1,4 +1,8 @@
 #/bin/sh -e
 rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT LIGO-WA
 rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-WA LIGO-CIT
+rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT-ARCHIVE LIGO-WA
+rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-WA LIGO-CIT-ARCHIVE
+rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT LIGO-CIT-ARCHIVE
+rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT-ARCHIVE LIGO-CIT
 
