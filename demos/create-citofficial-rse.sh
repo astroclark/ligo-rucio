5d4b2946f8cdac534cfbdfc3708234e0bd668535
James Clark <james.clark@ligo.org>
2018-10-04 10:37:50 -0400

    setting up file registration

--- demos/create-citofficial-rse.sh
+++ demos/create-citofficial-rse.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Create RSE
-rucio-admin rse add LIGO-CIT-OFFICIAL
+rucio-admin rse add LIGO-CIT-ARCHIVE
 
 # Define copying protocol
 rucio-admin rse add-protocol\
@@ -10,16 +10,16 @@ rucio-admin rse add-protocol\
     --domain-json '{"wan": {"read": 1, "write": 0, "delete": 0, "third_party_copy": 1}}' \
     --scheme gsiftp \
     --port 2811 \
-    LIGO-CIT-OFFICIAL
+    LIGO-CIT-ARCHIVE
 
 
 # FTS configuration
-rucio-admin rse set-attribute --rse LIGO-CIT-OFFICIAL --key fts --value https://fts.mwt2.org:8446
-rucio-admin rse set-attribute --rse LIGO-CIT-OFFICIAL --key fts_testing --value https://fts.mwt2.org:8446
+rucio-admin rse set-attribute --rse LIGO-CIT-ARCHIVE --key fts --value https://fts.mwt2.org:8446
+rucio-admin rse set-attribute --rse LIGO-CIT-ARCHIVE --key fts_testing --value https://fts.mwt2.org:8446
 
 # Disk quota
-rucio-admin -a root account set-limits root LIGO-CIT-OFFICIAL 1000000000000
+rucio-admin -a root account set-limits root LIGO-CIT-ARCHIVE 1000000000000
 
 # Make this part of the LIGO lab network
-rucio-admin rse set-attribute --rse LIGO-CIT-OFFICIAL --key ligo_lab --value True
+rucio-admin rse set-attribute --rse LIGO-CIT-ARCHIVE --key archive --value True
 
--- demos/register_dataset
+++ demos/register_dataset
@@ -3,7 +3,7 @@
 # Get ascii list of frames with e.g., ls -d -1 $PWD/**/*gwf.  Ideally use DiskCache.
 # Better yet: find $PWD -type f -name *gwf
 ligo_register_dataset \
-    --rse LIGO-CIT-OFFICIAL \
+    --rse LIGO-CIT-ARCHIVE \
     --scope O1 \
     --dataset-name L-L1_HOFT_C00-11370 \
     --debug --verbose \
