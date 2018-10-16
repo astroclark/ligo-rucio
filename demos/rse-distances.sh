8bed622e7d7a7d318d849792a79adc28f37b2588
James Clark <james.clark@ligo.org>
2018-10-16 14:03:02 -0400

    attrs

--- demos/rse-distances.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh -e
-rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT LIGO-LHO
-rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-LHO LIGO-CIT
-rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT-ARCHIVE LIGO-LHO
-rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-LHO LIGO-CIT-ARCHIVE
-rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT LIGO-CIT-ARCHIVE
-rucio-admin rse add-distance --distance 1 --ranking 1 LIGO-CIT-ARCHIVE LIGO-CIT
-
