7d1d46021da7e2df05156fa46e877fb27f496698
James Clark <james.clark@ligo.org>
2018-11-14 14:35:22 -0800

    upload

--- demos/upload-file.sh
+++ demos/upload-file.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -e
+
+# residuals frames
+h1=/home/sudarshan.ghonge/Projects/TGR/GW150914/IMRPhenomPv2/residual_frame/H-H1_HOFT_RES-1126259454-16.gwf
+l1=/home/sudarshan.ghonge/Projects/TGR/GW150914/IMRPhenomPv2/residual_frame/L-L1_HOFT_RES-1126259454-16.gwf
+
 rucio -v upload \
-  --scope TEST --rse LIGO-CIT \
-  --name H-H1_HOFT_C02-1137242112-4096.gwf\
-  --protocol gsiftp \
-  /hdfs/frames/O1/hoft_C02/H1/H-H1_HOFT_C02-11372/H-H1_HOFT_C02-1137242112-4096.gwf
+    --summary \
+    --scope RESIDUALS --rse LIGO-CIT \
+    --protocol gsiftp \
+    ${h1} ${l1}
+  #--name H-H1_HOFT_C02-1137242112-4096.gwf\
   #--pfn gsiftp://ldas-pcdev2.ligo.caltech.edu:2811/home/jclark/Projects/ligo-rucio/data/TEST/test.gwf \
   #--no-register 
